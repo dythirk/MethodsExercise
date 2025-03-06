da589c5aa241416d1565ee1d2b823e5175917d6b
Thai Vo <tahrbo@gmail.com>
2025-03-06 04:06:38 -0600

    First submittal

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,27 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("Please enter a noun: ");
+            Console.WriteLine("Please enter a noun:");
             string noun = Console.ReadLine(); // input
 
-            Console.WriteLine("Please enter a verb, for example 'I like to ... ' ");
+            Console.WriteLine("Please enter a verb, for example 'I like to ... '");
             string verb1 = Console.ReadLine();
 
-            Console.WriteLine("Please enter another verb: ");
+            Console.WriteLine("Please enter another verb:");
             string verb2 = Console.ReadLine();
 
-
-            Console.WriteLine("Please enter a location: ");
+            Console.WriteLine("Please enter a location:");
             string place = Console.ReadLine();
 
-            Console.WriteLine("Please enter a day of the week: ");
+            Console.WriteLine("Please enter a day of the week:");
             string day = Console.ReadLine();
 
-            Console.WriteLine($"Every {day} we go to the park to watch the {noun} {verb1}. After that we go to the {place} to {verb2}.");
+            Console.WriteLine($"In Wonderland, every {day} we go to the park to watch the {noun} {verb1}. " +
+                $"After that we go to the {place} to {verb2}.");
 
 
+            Console.WriteLine("Please enter an integer:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another integer:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"{a} plus {b} equals:");
+            Console.WriteLine(Add(a, b));
+
+            Console.WriteLine($"{a} minus {b} equals:");
+            Console.WriteLine(Subtract(a, b));
+
+            Console.WriteLine($"{a} times {b} equals:");
+            Console.WriteLine(Multiply(a, b));
+
+            Console.WriteLine($"{a} divided by {b} equals:");
+            Console.WriteLine(Divide(a, b));
+
+
+            static int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            static int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+
+            static int Subtract(int a, int b)
+            {
+                return a - b;
+            }
+
+            static int Divide(int a, int b)
+            {
+                return a / b;
+            }
         }
     }
 }
