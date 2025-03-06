32493368ea7c704a82b831ef0d6948f828c9aa89
Thai Vo <tahrbo@gmail.com>
2025-03-06 03:27:50 -0600

    Starting point

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Please enter a noun: ");
+            string noun = Console.ReadLine(); // input
+
+            Console.WriteLine("Please enter a verb, for example 'I like to ... ' ");
+            string verb1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter another verb: ");
+            string verb2 = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a location: ");
+            string place = Console.ReadLine();
+
+            Console.WriteLine("Please enter a day of the week: ");
+            string day = Console.ReadLine();
+
+            Console.WriteLine($"Every {day} we go to the park to watch the {noun} {verb1}. After that we go to the {place} to {verb2}.");
+
+
         }
     }
 }
